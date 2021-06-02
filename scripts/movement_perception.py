c7cdc5d027c86bac2a317bf2a55b918106d9d475
Michael Linden Su <michaelsu@cloyster.cs.uchicago.edu>
2021-06-02 13:54:46 -0500

    saving finished simple turn and straight movement

--- scripts/movement_perception.py
+++ scripts/movement_perception.py
@@ -11,7 +11,8 @@ from sensor_msgs.msg import LaserScan
 INITIALIZING = "initializing"
 INITIALIZED = "initialized"
 MOVE_FORWARD = "move_forward"
-SIMPLE_TURN = "simple_turn"
+SIMPLE_TURN_LEFT = "simple_turn_left"
+SIMPLE_TURN_RIGHT = "simple_turn_right"
 ACTION_TURN = "action_turn"
 CHECK_STATE = "check_state"
 CHECK_DISTANCE = "check_distance"
@@ -20,7 +21,6 @@ class MovementPerception(object):
     def __init__(self):
         rospy.init_node("movement_perception")
 
-        print("\nINITIALIZING\n")
         self.robot_state = INITIALIZING
 
         self.q_matrix_path = os.path.join(os.path.dirname(__file__), '../output/q_matrix.csv')
@@ -33,26 +33,34 @@ class MovementPerception(object):
         self.distances_old = [0.0, 0.0, 0.0]
         self.distances = [0.0, 0.0, 0.0]
         self.front = [358, 359, 0, 1, 2]
-        self.right = [88, 89, 90, 91, 92]
-        self.left = [268, 269, 270, 271, 272]
+        self.left = [88, 89, 90, 91, 92]
+        self.right = [268, 269, 270, 271, 272]
         self.distances_loaded = False
         
         self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 10)
         self.scan_sub = rospy.Subscriber('/scan', LaserScan, self.scan_distances)
 
-        print("\nINITIALIZED\n")
+        self.lin_vel = 0.0
+        self.ang_vel = 0.0
+
         self.robot_state = INITIALIZED
 
 
     def get_actions(self):
         return
 
+    def update_old_distances(self):
+        for i in range(len(self.distances)):
+            self.distances_old[i] = self.distances[i]
+
+
 
     def scan_distances(self, data):
-        if self.robot_state != CHECK_DISTANCE
+        if self.robot_state != CHECK_DISTANCE:
             return
 
-        self.distances_old = self.distances
+        self.update_old_distances()
+
         self.drm = data.range_max
 
         self.distances[0] = self.drm
@@ -62,24 +70,24 @@ class MovementPerception(object):
 
         self.distances[1] = self.drm
         for i in self.right:
-            if data.ranges[i] < d:
+            if data.ranges[i] < self.distances[1]:
                 self.distances[1] = data.ranges[i]
 
         self.distances[2] = self.drm
         for i in self.left:
-            if data.ranges[i] < d:
+            if data.ranges[i] < self.distances[2]:
                 self.distances[2] = data.ranges[i]
 
         self.robot_state = CHECK_STATE
 
 
     def check_robot_state(self):
-        d0 = distances[0]
-        d1 = distances[1]
-        d2 = distances[2]
-        d0_ref = d0 * 3
-        d1_ref = d1 * 3
-        d2_ref = d2 * 3
+        d0 = self.distances[0]
+        d1 = self.distances[1]
+        d2 = self.distances[2]
+        d0_ref = d0 * 2.5 
+        d1_ref = d1 * 2.5 
+        d2_ref = d2 * 2.5
 
         # if two openings: 
         #   self.robot_state = ACTION_TURN
@@ -91,38 +99,58 @@ class MovementPerception(object):
             self.robot_state = MOVE_FORWARD
 
 
-    def set_velocity(self, linear_vel, angular_vel):
+    def set_velocity(self):
         # Helper to set robot velocity
         msg = Twist()
-        msg.linear.x = linear_vel
-        msg.angular.z = angular_vel
+        msg.linear.x = self.lin_vel
+        msg.angular.z = self.ang_vel
         self.cmd_pub.publish(msg)
 
 
     def move_forward(self):
-        lin_vel = 0.2
-        ang_vel = 0.0
-        
-        if distances[1] < distances_old[1]:
-            # angled to right, turn slightly left
-            ang_vel = -0.2 * (distances_old[1] - distances[1])
-        elif distances[2] < distances_old[2]:
-            # angled to left, turn slightly right
-            ang_vel = 0.2 * (distances_old[2] - distances[2])
+        self.lin_vel = 0.15
 
-        self.set_velocity(lin_vel, ang_vel)
+        if self.distances[1] < self.distances_old[1]:
+            # angled to left, turn slightly right 
+            self.ang_vel = 2 * (self.distances_old[1] - self.distances[1])
+        elif self.distances[2] < self.distances_old[2]:
+            # angled to right, turn slightly left 
+            self.ang_vel = -4 * (self.distances_old[2] - self.distances[2])
+        self.set_velocity()
+        rospy.sleep(.25)
 
 
     def do_turn(self, direction):
         # Turn robot in-place in given direction, then move forward one tile
-        if direction == "left":
-            print("left turn")
-            rospy.sleep(100)
-        elif direction == "right":
-            print("right turn")
-            rospy.sleep(100)
-        else:
-            print("error: pass 'right' or 'left' direction to turn()")
+        if self.ang_vel != 0:
+            self.lin_vel = .15
+            self.ang_vel = 0
+            rospy.sleep(.5)
+            self.lin_vel = 0
+            self.set_velocity()
+            rospy.sleep(1)
+
+        self.lin_vel = 0
+        self.ang_vel = math.pi / 8 
+
+        if direction == "right":
+            self.ang_vel *= -1
+
+        self.set_velocity()
+        rospy.sleep(3.9)
+        self.ang_vel = 0
+        self.set_velocity()
+        rospy.sleep(1)
+        self.lin_vel = .15
+        self.set_velocity()
+        rospy.sleep(6)
+        self.lin_vel = 0
+        self.set_velocity()
+        rospy.sleep(1)
+
+        self.robot_state = CHECK_DISTANCE
+        while self.robot_state == CHECK_DISTANCE:
+            pass
 
 
     def do_action(self):
